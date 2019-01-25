6227e5e5c74b6fc3304ed154493f59a8d233d070
Edlar <murmurcuka@gmail.com>
2019-01-25 21:25:58 +0600

    Конечный продукт 1.0

--- app/src/main/java/com/example/myprojectandroid/MainActivity.java
+++ app/src/main/java/com/example/myprojectandroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -424,5 +425,17 @@ public class MainActivity extends AppCompatActivity{
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_memu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.developer) {
+            Toast.makeText ( MainActivity.this, "Made by developer Akkozov Eldar", Toast.LENGTH_LONG ).show ();
+        }
+
+        return super.onOptionsItemSelected (item);
     }
 }
